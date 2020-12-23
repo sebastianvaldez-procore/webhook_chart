77fbadae8623c93af49ee0afac83ddd16a94f05b
sebastian valdez <sebastian.valdez@procore.com>
2020-12-22 23:06:32 -0800

    Unwrap Route components from Grid, Now only Header is in Grid

--- src/App.js
+++ src/App.js
@@ -3,23 +3,29 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid';
-
+import { makeStyles } from '@material-ui/core/styles';
 import { Chart } from './Chart';
 import PayloadField from './PayloadField';
 
-
+const useStyles = makeStyles(theme => ({ 
+  root: {
+    flexGrow: 1
+  }
+}))
 
 function App() {
+  const classes = useStyles()
   return (
     <>
       <CssBaseline />
-      <Grid container justify='center' alignItems="center" spacing={2}>
-
+      <div className={classes.root}>
+      <Grid container justify='center' alignItems="center" spacing={1}>
         <Grid item xs={12} md={12} lg={12}>
           <Typography align='center' variant="h3">
           Chart Procore Webhook Deliveries
           </Typography>
         </Grid>
+      </Grid>
 
         <Router basename={process.env.PUBLIC_URL}>
           <Switch>
@@ -28,7 +34,7 @@ function App() {
             <Route render={ () => <Typography variant='h2'>404 Not found.</Typography>} />
           </Switch>
         </Router> 
-      </Grid>
+      </div>
     </>
   );
 }
